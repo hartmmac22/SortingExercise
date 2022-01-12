346a3a286f2e0e5b500ff464ce7efe85874f8603
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2022-01-12 08:41:21 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,10 +1,10 @@
+//very confused and couldnt get this to compile and run the program
 public class Tester {
-
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
     SortingAlgorithms s1 = new SortingAlgorithms;
     //create an int[] of length 100;
-    //  private int[] arr = int[100];
+	private int[] arr = int[100];
     //									{x,y,z,....};
     s1.bubbleSort();
     s1.insertionSort();  //print # swaps for each
